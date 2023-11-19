7986893f9f09efc4a1d49d198cbc01e3f87deb2d
Olga Kopaeva <olya.ant@gmail.com>
2023-11-19 20:29:25 +0300

    Task is done.

--- src/main/java/task/example/KopaevaTestTaskJava.java
+++ src/main/java/task/example/KopaevaTestTaskJava.java
@@ -1,14 +1,11 @@
 package task.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class TestTask {
+public class KopaevaTestTaskJava {
     private static final int CONST = 7;
     private static final String GIVEN_NAME = "Вячеслав";
-    private static final int[] INT_ARRAY = {0, 1, 3, 10, 12, 14, 24, 33, 67};
-
-    private static final String BRACKET_SEQUENCE = "[((())()(())]]";
-    //This bracket sequence is not correct, because 1 bracket "(" is not closed with ")". For change it to correct, we should change 1 bracket "]" into ")".
 
     public static void main(String[] args) {
 
@@ -22,9 +19,15 @@ public class TestTask {
         String name = newName.nextLine();
         checkName(name);
 
-        checkArrayElementsMultipleOf3(INT_ARRAY);
+        Scanner newArray = new Scanner(System.in);
+        System.out.println("Введите числовой массив, разделяя числа пробелами: ");
+        String arrString = newArray.nextLine();
+        String[] arrayStrings = arrString.split(" ");
+        int[] arrayNumbers = Arrays.stream(arrayStrings)
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        checkArrayElementsMultipleOf3(arrayNumbers);
 
-        System.out.println("Bracket Sequence is correct? - " + ifSequenceIsCorrect(BRACKET_SEQUENCE));
     }
 
     public static void checkNumber(int number) {
@@ -42,32 +45,9 @@ public class TestTask {
     }
 
     public static void checkArrayElementsMultipleOf3(int[] inputArray) {
+        System.out.println("Числа, кратные 3:");
         for (int i : inputArray)
             if (i % 3 == 0) System.out.println(i + " ");
     }
-
-    public static boolean ifSequenceIsCorrect(String sequence) {
-        boolean flag = false; //Is sequence open?
-        int list1 = 0; //number of unpair (
-
-        for (int i = 0; i < sequence.length(); i++) {
-            if (sequence.charAt(i) == '(')
-                if (!flag) {
-                    flag = true;
-                    ++list1;
-                } else
-                    ++list1;
-
-            if (sequence.charAt(i) == ')' && !flag) {
-                return false;
-            }
-            if (sequence.charAt(i) == ')' && flag) {
-                --list1;
-                if (list1 == 0) flag = false;
-            }
-
-        }
-        return !flag;
-    }
-
 }
+
